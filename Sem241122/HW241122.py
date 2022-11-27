9c1ae91fef3198748d5846af569c780cf9afb28b
HunterM <zakaspro@gmail.com>
2022-11-27 20:40:02 +0300

    Промежуточный итог 271122

--- Sem241122/HW241122.py
+++ Sem241122/HW241122.py
@@ -1,19 +1,21 @@
 # 1. Напишите программу, которая принимает на
-# вход вещественное число и показывает сумму его цифр.
+# вход вещественное число и показывает сумму 
+# его цифр.
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
 
-# print('Введите вещественное число')
-# number =  float(input())
-# str1=str(number)
-# # print(str1)
-# sum=0
-# for element in str1:
-#     if element.isdigit():
-#         sum += int(element)
-#         print(element)
-# print("k=" ,sum)
+print('Введите вещественное число:\n')
+number =  float(input())
+str1=str(number)
+print(str1)
+sum=0
+for element in str1:
+    if element.isdigit():
+        sum += int(element)
+        print(element)
+print("Сумма цифр равна: " ,sum)
+
 
 
 # 2. Напишите программу, которая принимает на
@@ -54,34 +56,37 @@
 # ввести позиции в консоли)
 #  -2 -1 0 1 2 Позиции: 0,1 -> 2
 
-print("Введите число N")
-print()
-n1 = int(input())
-str3 = list(range(- n1, n1+1))
-s = len(str3)
-proiz = 0
-# print(str3)
-# print(len(str3))
-print("Введите позиции для подсчета из диапазона от 0 до", s-1)
-poz1 = list(map(int, input().split()))
-# poz2 = int(input())
-# print(str3[poz1])
-# print(str3[poz2])
-
-for i in range(0, s-1):
-    if s<poz1 or poz1<0 or 0>poz2 or poz2>s:
-        print("Элементы вне допустимого диапазона")
-    elif poz1>0 or poz1<s or poz2<s and poz2>0:
-        proiz = str3[poz1]*str3[poz1]
-        print('Результат равен: ', proiz)
-        break
-
-
-
-# 5.Реализуйте алгоритм перемешивания списка.
+# path = 'THW241122.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+# exit()
 
 # print("Введите число N")
 # print()
-# numberN = int(input())
-# str2 = list(range(1, numberN+1))
-# print()
+# n1 = int(input())
+# str3 = list(range(- n1, n1+1))
+# s = len(str3)
+# proiz = 0
+# print(str3)
+# print(len(str3))
+# print("Введите позиции для подсчета из диапазона от 0 до", s-1)
+# poz1 = int(input())
+# poz2 = int(input())
+
+
+# for i in range(0, s-1):
+#     if s<poz1 or poz1<0 or 0>poz2 or poz2>s:
+#         print("Элементы вне допустимого диапазона")
+#     elif poz1>0 or poz1<s or poz2<s and poz2>0:
+#         proiz = str3[poz1]*str3[poz2]
+#         print('Результат равен: ', proiz)
+#         break
+
+# 5.Реализуйте алгоритм перемешивания списка.
+
+# import random 
+# listRandom = ['a', 'b', 'c', 'd', 'e'] 
+# random.shuffle(listRandom) 
+# print(listRandom)
