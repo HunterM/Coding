7f90580b8eb358c26cfd55309743d260b8330adf
HunterM <zakaspro@gmail.com>
2022-11-27 22:44:11 +0300

    создание текстового файла

---
+++ Sem241122/txtformatting.py
@@ -0,0 +1,38 @@
+with open('THW241122.txt', 'w') as data:
+    data.write('0\n')
+    data.write('1\n')
+    data.write('2\n')
+    data.write('3\n')
+    data.write('4\n')
+    data.write('5\n')
+    data.write('6\n')
+    data.write('7\n')
+    data.write('8\n')
+    data.write('9\n')
+    data.write('10\n')
+
+    
+# colors = ['red', 'green', 'yellow']
+# data = open('HW221122.txt', 'a')
+# # data.writelines(colors)
+# data.write('\nLine1\n')
+# data.write('Line1\n')
+
+# data.close()
+
+
+# path = 'THW241122.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# def concatinatio(*params):
+#     # res: str = "" # склеивает строку
+#     res = 1 #склеивает числа
+
+#     for item in params:
+#         res *= item
+#     return res
+
+# exit()
