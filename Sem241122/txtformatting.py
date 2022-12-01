044885bcb17294f304f4d97d4dcd25001ac8872b
HunterM <zakaspro@gmail.com>
2022-12-01 18:49:49 +0300

    делаем текстовый файл

--- Sem241122/txtformatting.py
+++ Sem241122/txtformatting.py
@@ -4,12 +4,7 @@ with open('THW241122.txt', 'w') as data:
     data.write('2\n')
     data.write('3\n')
     data.write('4\n')
-    data.write('5\n')
-    data.write('6\n')
-    data.write('7\n')
-    data.write('8\n')
-    data.write('9\n')
-    data.write('10\n')
+ 
 
     
 # colors = ['red', 'green', 'yellow']
