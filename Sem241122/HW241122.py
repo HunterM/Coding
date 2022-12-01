240334cc34dd781d567c1b0f580d38bc837a6023
HunterM <zakaspro@gmail.com>
2022-12-01 18:49:08 +0300

    добавлена 4 задача

--- Sem241122/HW241122.py
+++ Sem241122/HW241122.py
@@ -71,49 +71,19 @@
 # ввести позиции в консоли)
 #  -2 -1 0 1 2 Позиции: 0,1 -> 2
 
-# path = 'THW241122.txt'
-# data = open(path, 'r')
-# for line in data:
-#     print(line) #занести в список
-# data.close()
-# # exit()
+with open('THW241122.txt', 'r') as f:
+    positions = f.read().split('\n')
+positions = list(map(int, positions))
 
+n = int(input())
+list_gen =[i for i in range(-n, n+1)]
+multi = 1
+for pos in positions:
+    multi*=list_gen[pos]
+print(positions)
+print(multi)
+print(list_gen)
 
-# print("Введите число N")
-# print()
-# n1 = int(input())
-# str3 = list(range(- n1, n1+1))
-# s = len(str3)
-
-# print(str3)
-# print(len(str3))
-# print("Введите позиции для подсчета из диапазона от 0 до", s-1)
-
-# a = list(map(int, input().split()))
-# print(a)
-# proiz = 0
-# for i in a:
-#     proiz *= str3[i]
-# print(proiz)
-# poz1 = int(input())
-# poz2 = int(input())
-
-# for i in range(0, s-1):
-#     if s < a[i] or a[i] < 0 or 0 > a[i] or a[i] > s:
-#         print("Элементы вне допустимого диапазона")
-    # elif poz1 > 0 or poz1 < s or poz2 < s and poz2 > 0:
-    #     proiz = str3[poz1]*str3[poz2]
-    #     print('Результат равен: ', proiz)
-    #     break
-
-
-# for i in range(0, s-1):
-#     if s < poz1 or poz1 < 0 or 0 > poz2 or poz2 > s:
-#         print("Элементы вне допустимого диапазона")
-#     elif poz1 > 0 or poz1 < s or poz2 < s and poz2 > 0:
-#         proiz = str3[poz1]*str3[poz2]
-#         print('Результат равен: ', proiz)
-#         break
 
 
 # 5.Реализуйте алгоритм перемешивания списка.
