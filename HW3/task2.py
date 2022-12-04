52cc83b4a8ea480766db7a40c73b2dd2a25c5575
HunterM <zakaspro@gmail.com>
2022-12-04 19:39:01 +0300

    Домашка к 3 уроку

---
+++ HW3/task2.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая найдёт
+# произведение пар чисел списка.
+# Парой считаем первый и последний элемент,
+# второй и предпоследний и т.д.
+
+import numpy as np 
+lst1 = np.random.randint(0, 10, 5)
+print(lst1)
+
+
+def proizved(nums: list) -> int:
+    newlist = []
+    iterations = int(round((len(nums))+1)/2)
+    print(iterations)
+    for i in range(iterations):
+        newlist.append(lst1[i]*lst1[len(lst1)-1-i])
+    return newlist
+
+
+print(proizved([lst1]))
