88903d31a5026191a2fc325a5efd4dadca6fbd7e
HunterM <zakaspro@gmail.com>
2022-12-14 22:32:38 +0300

    добавлен logger скорректирован список User

--- HW7/ModelBook.py
+++ HW7/ModelBook.py
@@ -1,57 +1,44 @@
-# def ModelBook():
 import os
 import random
 import csv
 import logger
 
 
-
 # print("Текущая деректория:", os.getcwd())
 
-ModelBook = open("UsersFile.csv", "w")
+file = open("UsersFile.csv", "w")
 NewString = "ID, NAME, TELEPHONE\n"
-ModelBook.write(NewString)
-# # ModelBook.write("In this place you can look instant contacts")
-# # ModelBook.close()
+file.writelines(NewString)
 
 
+name_users = ['UserA', 'UserB', 'UserC', 'UserD', 'UserI', 'UserF', 'UserH', 'UserH',
+              'UserW', 'UserY']
+second_name = ['NameA', 'NameB', 'NameC', 'NameD',
+               'NameI', 'NameF', 'NameH', 'NameK', 'NameN', 'NameO']
 
-# with open('UsersFile.txt', 'w') as data:
-#     data.write('User1\n')
-#     data.write('User2\n')
-#     data.write('User3\n')
-#     data.write('User4\n')
-#     data.write('User5\n')
-#     data.write('User6\n')
-#     data.write('User7\n')
-#     data.write('User8\n')
-#     data.write('User9\n')
-#     data.write('User10\n')
-# ModelBook.close()
-name_users = ['UserA', 'UserB', 'UserC', 'UserD', 'UserI', 'UserF', 'UserH', 'UserH',\
-            'UserW', 'UserY']
-second_name = ['NameA', 'NameB', 'NameC', 'NameD',\
-                 'NameI', 'NameF', 'NameH', 'NameK', 'NameN', 'NameO']
 
 def list_of_numbers():
     randomPhone = random.randint(7900000000, 8000000000)
     return str(randomPhone)
 
+
 def skleyka_strok():
     skl = ""
-    skl = skl + random.choice(second_name) + ','+ random.choice(name_users) + ',' + random.choice(list_of_numbers) + ',' + random.choice(second_name)
+    skl = skl + random.choice(second_name) + ',' + random.choice(name_users) + list_of_numbers()
     return skl
+
 
 def start():
     for i in range(20):
-        stroka = f'{str(i+1)}, {skleyka_strok}\n'
-        ModelBook.write(stroka)
-    ModelBook.close()
+        stroka = f'{str(i + 1)},{skleyka_strok()}\n'
+        file.write(stroka)
+    file.close()
+
 
 start()
 
 os.replace("UsersFile.csv", "D:/Coding/Coding/HW7/UsersFile.csv")
-ModelBook.close()
+file.close()
 
 # os.replace("UsersFile.txt", "D:\Coding\Coding\HW7\UsersFile.txt")
 # ModelBook.close()
@@ -62,5 +49,3 @@ ModelBook.close()
 #     users_names = []
 #     users_names.append(users_names1)
 #     print(users_names)
-
-
--- HW7/logger.py
+++ HW7/logger.py
@@ -1,2 +1,16 @@
 import logging
+import os
 
+path = "D:/Coding/Coding/HW7/"
+logging.basicConfig(filename=os.path.join(path, 'logger.log'),level=logging.DEBUG)
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='logger.log',
+    format= "%(asctime)s - [%(levelname)s] - %(app)s - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
+    datefmt='%d.%m.%Y %H:%M',
+                    )
+
+
+
+# os.replace("logger.log", "D:/Coding/Coding/HW7/logger.log")
