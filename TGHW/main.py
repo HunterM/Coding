9ff7324ecef57dde907992d293fdcf722104bd35
HunterM <zakaspro@gmail.com>
2022-12-26 19:16:05 +0300

    I need HELP

--- TGHW/main.py
+++ TGHW/main.py
@@ -69,6 +69,9 @@ app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
 app.add_handler(CommandHandler("help", help_command))
 app.add_handler(CommandHandler("sum", sum_command))
+app.add_handler(CommandHandler("game", sum_command))
 
 print('Server started')
+
+
 app.run_polling()
