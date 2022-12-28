d6aa34464d651eec2f86a0ff4d889eeae0021ee3
HunterM <zakaspro@gmail.com>
2022-12-29 00:45:09 +0300

    ....

--- ExChange/main.py
+++ ExChange/main.py
@@ -23,18 +23,19 @@ def currency(message):
         if message.text == '€':
                 msg = bot.send_message(message.chat.id, 'Вы выбрали евро') 
                 bot.send_message(message.chat.id, EUR)
+                bot.send_message(message.chat.id, 'Введите /start для повтора операции')
         elif message.text == '$':
                 msg = bot.send_message(message.chat.id, 'Вы выбрали доллары')
                 bot.send_message(message.chat.id, USD)
+                bot.send_message(message.chat.id, 'Введите /start для повтора операции')
         elif message.text == 'Y':
                 msg = bot.send_message(message.chat.id, 'Вы выбрали юани')
                 bot.send_message(message.chat.id, CNY)
+                bot.send_message(message.chat.id, 'Введите /start для повтора операции')
         
         else:
                 msg = bot.send_message(message.chat.id, 'Введите корректные данные')
                 bot.register_next_step_handler(msg, currency)
-
-
 
 bot.polling()
        
